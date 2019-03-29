60e373cf12f27573b53c28705ee7d2a21563a2e4
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2019-03-29 16:46:03 -0700

    Adding startup class

--- src/DependencyInjection/src/DependencyInjection/IFunctionsStartup.cs
+++ src/DependencyInjection/src/DependencyInjection/IFunctionsStartup.cs
@@ -4,6 +4,8 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Microsoft.Azure.Functions.Extensions.DependencyInjection
@@ -12,14 +14,19 @@ namespace Microsoft.Azure.Functions.Extensions.DependencyInjection
     /// Interface defining a startup configuration action that should be performed
     /// as part of the Functions runtime startup.
     /// </summary>
-    public interface IFunctionsStartup
+    public abstract class FunctionsStartup : IWebJobsStartup
     {
+        public void Configure(IWebJobsBuilder builder)
+        {
+            
+        }
+
         /// <summary>
         /// Performs the startup configuration action. The runtime will call this
         /// method at the right time during initialization.
         /// </summary>
         /// <param name="builder">The <see cref="IWebJobsBuilder"/> that can be used to
         /// configure the host.</param>
-        void Configure(IFunctionsHostBuilder builder);
+        public abstract void Configure(IFunctionsHostBuilder builder);
     }
 }
