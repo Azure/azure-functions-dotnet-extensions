146b6a8c8910b6381930f8e06ec5a1ec334cfdfc
Fabio Cavalcante <fabio.cavalcante@microsoft.com>
2019-04-18 14:01:57 -0700

    Adding basic DI sample

---
+++ src/samples/DependencyInjection/SampleFunction.cs
@@ -0,0 +1,43 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace DependencyInjection
+{
+    public class SampleFunction
+    {
+        private readonly IGreeter _greeter;
+
+        public SampleFunction(IGreeter greeter)
+        {
+            _greeter = greeter;
+        }
+
+        [FunctionName("SampleFunction")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            return name != null
+                ? (ActionResult)new OkObjectResult(_greeter.CreateGreeting(name))
+                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+        }
+    }
+}
