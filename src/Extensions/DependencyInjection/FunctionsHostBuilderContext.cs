27b2f326e4552acfdafd880eb9378bc8ad973c23
Brett Samblanet <brettsam@microsoft.com>
2020-05-19 08:49:24 -0700

    adding comments

--- src/Extensions/DependencyInjection/FunctionsHostBuilderContext.cs
+++ src/Extensions/DependencyInjection/FunctionsHostBuilderContext.cs
@@ -7,6 +7,9 @@ using Microsoft.Extensions.Configuration;
 
 namespace Microsoft.Azure.Functions.Extensions.DependencyInjection
 {
+    /// <summary>
+    /// Context containing the common services and settings for the Functions host.
+    /// </summary>
     public abstract class FunctionsHostBuilderContext
     {
         private readonly WebJobsBuilderContext _webJobsBuilderContext;
@@ -16,10 +19,19 @@ namespace Microsoft.Azure.Functions.Extensions.DependencyInjection
             _webJobsBuilderContext = webJobsBuilderContext ?? throw new ArgumentNullException(nameof(webJobsBuilderContext));
         }
 
+        /// <summary>
+        /// Gets the name of the environment. This is "Development" when running in the Azure Functions Core Tools and "Production" when deployed to Azure.
+        /// </summary>
         public string EnvironmentName => _webJobsBuilderContext.EnvironmentName;
 
+        /// <summary>
+        /// Gets the <see cref="IConfiguration"/> containing the merged configuration of the application and the host.
+        /// </summary>
         public IConfiguration Configuration => _webJobsBuilderContext.Configuration;
 
+        /// <summary>
+        /// Gets or sets the absolute path to the directory that contains the application content files.
+        /// </summary>
         public string ApplicationRootPath => _webJobsBuilderContext.ApplicationRootPath;
     }
 }
